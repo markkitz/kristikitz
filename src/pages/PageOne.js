f7927bdafa1b83d64dc292d9030c8b400a61bd1b
Mark Kitz <MarkK@nait.ca>
2020-03-15 16:39:44 -0600

    after publish

--- src/pages/PageOne.js
+++ src/pages/PageOne.js
@@ -10,18 +10,24 @@ export default () => {
         the greater Edmonton metropolitan area. There's an internal application
         used to manage all the data, and an external application for clients to
         use. I worked on the latter, and my contributions to the project are depicted below. The
-        application is built with React and Google FireBase. I did most of the
+        application is built with React and Google Firebase. I did most of the
         design work, using Adobe XD to prototype, and part of the front-end development.
       </p>
       <h3>Search Modal</h3>
-      <p>Modal window to search for properties. </p>
+      <p>Modal window to search for properties based on size and location. </p>
       <img src="/images/search.gif" />
+      <h3>Saved Searches</h3>
+      <p>Clients can save and share custom searches.</p>
+      <img src="images/savedsearch.png"/>
       <h3>Search Results</h3>
-      <p>Search results dispalyed in a list and on a map.</p>
+      <p>Search results displayed in list form and on an interactive map.</p>
       <img src="/images/searchresults.gif"/>
-      <a href="https://kristi-kitz.firebaseapp.com/">
-        https://kristi-kitz.firebaseapp.com
-      </a>
+      <h3>Listing Page</h3>
+      <p>Listing page for the parcel of land.</p>
+      <img src="/images/listingpage.png" style={{border: "1px solid #ddd"}}/>
+      <h3>404 Page</h3>
+      <p>Custom 404 page - I designed the graphic!</p>
+      <img src="/images/404.png" style={{border: "1px solid #ddd"}}/>
     </Container>
   )
 }
@@ -29,4 +35,15 @@ export default () => {
 const Container = styled.div`
   width: 1000px;
   margin: auto;
+  padding-top: 10px;
+  padding-bottom: 60px;
+  & > h1, h3 {
+    font-weight: 400;
+  }
+  & > h3 {
+    border-bottom: 2px solid #ddd;
+    padding-bottom: 5px;
+  }
 `
+
+
